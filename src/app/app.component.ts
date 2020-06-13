c0be02fe6134481f0ca8bfc10b0ffa9279b603c0
Marcelo Reis <oi@marcelreis.dev>
2020-06-13 18:24:30 -0300

    feat: improvement of navigation tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component, ViewEncapsulation } from '@angular/core'
 })
 export class AppComponent {
   title = 'summoners-rift'
+
+  constructor(public router: Router) {}
 }
