838bceb35571a8b9c6e63f14e7c632077126639f
Marcelo Reis <oi@marcelreis.dev>
2020-06-13 22:52:02 -0300

    refactor: improvement in reducer state controll

--- src/app/models/match.ts
+++ src/app/models/match.ts
@@ -1,4 +1,8 @@
-export type OpponentType = { id: number; score: number } | null
+export type OpponentType = {
+  id: number
+  score: number
+  winner?: boolean
+} | null
 export class Match {
-  constructor(public teams: OpponentType[]) {}
+  constructor(public teams: OpponentType[], public ended?: boolean) {}
 }
--- src/app/tournament/store/tournament.reducer.ts
+++ src/app/tournament/store/tournament.reducer.ts
@@ -1,10 +1,7 @@
-import { map } from 'rxjs/operators'
 import { Action, createReducer, on } from '@ngrx/store'
 import * as TournamentActions from './tournament.action'
 
-import { Team } from './../../models/team'
 import { Match } from 'src/app/models/match'
-import { pathToFileURL } from 'url'
 
 const enum CurrentState {
   'uninitialized',
@@ -55,50 +52,61 @@ const scoreboardReducer = createReducer(
   on(
     TournamentActions.addScore,
     (state, { phaseIndex, matchIndex, teamId }) => {
-      let score
+      let selectedMatch = state.matches[phaseIndex][matchIndex]
+
+      const invalidMatch = () =>
+        selectedMatch.ended ||
+        selectedMatch.teams.some((team) => !team || !team.id)
+
+      if (invalidMatch()) {
+        return { ...state }
+      }
 
       return {
         ...state,
         matches: state.matches.map((phase, index) => {
           if (index === phaseIndex) {
             phase = phase.map((match, index) => {
-              if (matchIndex === index) {
-                match = new Match(
-                  match.teams.map((team) => {
-                    if (team.id === teamId) {
-                      score = team.score + 1
-                      return {
-                        id: team.id,
-                        score,
-                      }
-                    }
-                    return team
-                  })
+              if (matchIndex === index && !match.ended) {
+                const replaceMatch = (team) =>
+                  team.id === teamId
+                    ? { id: team.id, score: team.score + 1 }
+                    : team
+
+                match = new Match(match.teams.map(replaceMatch), match.ended)
+
+                const ended = match.teams.find(
+                  (team) => team.score > state.bestOf / 2
                 )
+                if (ended) {
+                  match = new Match(
+                    match.teams.map((team) => {
+                      return { ...team, winner: team.score > state.bestOf / 2 }
+                    }),
+                    true
+                  )
+                }
+                selectedMatch = match
               }
               return match
             })
           }
 
-          if (index === phaseIndex + 1) {
-            phase = phase.map((match, index) => {
-              if (Math.floor(matchIndex / 2) === index) {
-                match = new Match(
-                  match.teams.map((team, index) => {
-                    if (
-                      !team &&
-                      score > state.bestOf / 2 &&
-                      matchIndex % 2 === index
-                    ) {
-                      return { id: teamId, score: 0 }
-                    }
+          const isSubsequentPhase =
+            selectedMatch.ended && index === phaseIndex + 1
+          if (isSubsequentPhase) {
+            const nextMatchIndex = Math.floor(matchIndex / 2)
+            const nextOpponentIndex = matchIndex % 2
 
-                    return team
-                  })
-                )
-              }
-              return match
-            })
+            const replaceOpponent = (team, index) =>
+              nextOpponentIndex === index ? { id: teamId, score: 0 } : team
+
+            const replaceMatch = (match, index) =>
+              nextMatchIndex === index
+                ? (match = new Match(match.teams.map(replaceOpponent), false))
+                : match
+
+            phase = phase.map(replaceMatch)
           }
           return phase
         }),
