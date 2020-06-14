e2b00c7a587d41bc36b4276c646a198a504d6e7e
Marcelo Reis <oi@marcelreis.dev>
2020-06-14 13:56:03 -0300

    fix: typo in displaying bestOf 3

--- src/app/register/tournament-form/tournament-form.component.ts
+++ src/app/register/tournament-form/tournament-form.component.ts
@@ -35,7 +35,7 @@ export class TournamentFormComponent {
     ]
     this.bestOfConfig = [
       { value: 1, viewValue: '1' },
-      { value: 3, viewValue: '2' },
+      { value: 3, viewValue: '3' },
       { value: 5, viewValue: '5' },
     ]
   }
