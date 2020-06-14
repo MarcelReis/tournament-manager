47f516a78ea89e2a20db223ad9e3807840f18861
Marcelo Reis <oi@marcelreis.dev>
2020-06-14 19:34:57 -0300

    feat: improves readability

--- src/app/register/tournament-form/tournament-form.component.ts
+++ src/app/register/tournament-form/tournament-form.component.ts
@@ -34,9 +34,9 @@ export class TournamentFormComponent {
       { value: 16, viewValue: '16 - Eighth-finals' },
     ]
     this.bestOfConfig = [
-      { value: 1, viewValue: '1' },
-      { value: 3, viewValue: '3' },
-      { value: 5, viewValue: '5' },
+      { value: 1, viewValue: '1 - Single Match' },
+      { value: 3, viewValue: '3 - Best of three' },
+      { value: 5, viewValue: '5 - Best of five' },
     ]
   }
 
